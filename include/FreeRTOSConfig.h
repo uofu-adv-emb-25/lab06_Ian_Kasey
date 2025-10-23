65464a74b8605334bc91ab3d17c68c537b0957a2
Kasey Kemp <113135546+Fille2797@users.noreply.github.com>
2025-10-23 16:28:22 -0600

    Updated priority inversion test and config file to support runtime stats.

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -3,6 +3,9 @@
 // This is defined for debugging/data collection: remove if needed
 #define configUSE_TRACE_FACILITY 1
 #define configGENERATE_RUN_TIME_STATS 1
+#define configRUN_TIME_COUNTER_TYPE uint64_t
+#define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() 
+#define portGET_RUN_TIME_COUNTER_VALUE()            time_us_64()
 
 // This example uses a common include to avoid repetition
 #include "FreeRTOSConfig_examples_common.h"
--- test/test.c
+++ test/test.c
@@ -6,7 +6,6 @@
 #include <unity.h>
 #include "unity_config.h"
 #include "semphr.h"
-#include "projdefs.h"
 
 
 #define TEST_RUNNER_PRIORITY      ( tskIDLE_PRIORITY + 10UL )
@@ -23,8 +22,7 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void higher_prio_task(void *params) {
-    // hard_assert(cyw43_arch_init() == PICO_OK);
-    SemaphoreHandle_t lock = (SemaphoreHandle_t)params;
+    SemaphoreHandle_t lock = (*(SemaphoreHandle_t*)params);
     while(1) {
         xSemaphoreTake(lock, 0xffff);
         for(uint32_t i = 0; i < 100000; i++) {;}
@@ -33,16 +31,13 @@ void higher_prio_task(void *params) {
 }
 
 void medium_prio_task(void *params) {
-    // hard_assert(cyw43_arch_init() == PICO_OK);
     while(1) {
         for(uint32_t i = 0; i < 100000; i++) {;}
     }
 }
 
 void lower_prio_task(void *params) {
-    // hard_assert(cyw43_arch_init() == PICO_OK);
     SemaphoreHandle_t lock = (*(SemaphoreHandle_t*)params);
-    printf("Lock Address: %x\n", &lock);
     while (1) {
         xSemaphoreTake(lock, 0xffff);
         for(uint32_t i = 0; i < 100000; i++) {;}
@@ -51,34 +46,60 @@ void lower_prio_task(void *params) {
 }
 
 void test_priority_inversion() {
-    TEST_MESSAGE("Starting test_priority_inversion.");
     SemaphoreHandle_t lock = xSemaphoreCreateBinary();
-    printf("Lock Address: %x\n", &lock);
+    xSemaphoreGive(lock);
     TaskHandle_t lower_task;
     TaskHandle_t medium_task;
     TaskHandle_t higher_task;
-    xTaskCreate(lower_prio_task, "LowerPriorityThread",
+    xTaskCreate(lower_prio_task, "LowerPrioTask",
                 LOWER_TASK_STACK_SIZE, (void *)&lock, LOWER_TASK_PRIORITY, &lower_task);
-    TEST_MESSAGE("Created lower_prio_task.");
-    vTaskDelay(pdMS_TO_TICKS(5));
-    TEST_MESSAGE("Delayed after lower prio.");
-    xTaskCreate(higher_prio_task, "HigherPriorityThread",
+    vTaskDelay(pdMS_TO_TICKS(1));
+    xTaskCreate(higher_prio_task, "HigherPrioTask",
                 HIGHER_TASK_STACK_SIZE, (void *)&lock, HIGHER_TASK_PRIORITY, &higher_task);
-    xTaskCreate(medium_prio_task, "MediumPriorityThread",
+    xTaskCreate(medium_prio_task, "MediumPrioTask",
                 MEDIUM_TASK_STACK_SIZE, NULL, MEDIUM_TASK_PRIORITY, &medium_task);
-    TEST_MESSAGE("Created other two tasks.");
     TaskStatus_t lower_task_status;
     TaskStatus_t medium_task_status;
     TaskStatus_t higher_task_status;
 
-    // vTaskDelay(0xff);
+    // Block for 1ms to allow HigherPrioTask & MediumPrioTask to run
+    vTaskDelay(pdMS_TO_TICKS(1));
+
+    // eNums for Task States:
+    // eRunning == 0
+    // eReady   == 1
+    // eBlocked == 2
 
     vTaskGetInfo(lower_task, &lower_task_status, pdFALSE, eInvalid);
-    TEST_MESSAGE("Got lower task info.");
-    while(1) {
-        printf("Lower Task Status:\nState: %d\nName: %s\nExecution Time: %lu\n", lower_task_status.eCurrentState, lower_task_status.pcTaskName, lower_task_status.ulRunTimeCounter);
-        vTaskDelay(pdMS_TO_TICKS(5000));
+    vTaskGetInfo(medium_task, &medium_task_status, pdFALSE, eInvalid);
+    vTaskGetInfo(higher_task, &higher_task_status, pdFALSE, eInvalid);
+    printf("Lower Task Status:\n\tState: %d\n\tName: %s\n\tExecution Time: %llu\n", lower_task_status.eCurrentState, lower_task_status.pcTaskName, lower_task_status.ulRunTimeCounter);
+    printf("Medium Task Status:\n\tState: %d\n\tName: %s\n\tExecution Time: %llu\n", medium_task_status.eCurrentState, medium_task_status.pcTaskName, medium_task_status.ulRunTimeCounter);
+    printf("Higher Task Status:\n\tState: %d\n\tName: %s\n\tExecution Time: %llu\n", higher_task_status.eCurrentState, higher_task_status.pcTaskName, higher_task_status.ulRunTimeCounter);
+
+    // Capture current runtime values
+    // For each 5ms interval:
+    //    lower should stay the same
+    //    medium should increase
+    //    higher should stay the same
+    // Showing priority inversion
+    uint64_t lower_prio_runtime = lower_task_status.ulRunTimeCounter;
+    uint64_t medium_prio_runtime = medium_task_status.ulRunTimeCounter;
+    uint64_t higher_prio_runtime = higher_task_status.ulRunTimeCounter;
+
+    for(int i = 0; i < 4; i++) {
+        vTaskDelay(pdMS_TO_TICKS(5));
+    vTaskGetInfo(lower_task, &lower_task_status, pdFALSE, eInvalid);
+    vTaskGetInfo(medium_task, &medium_task_status, pdFALSE, eInvalid);
+    vTaskGetInfo(higher_task, &higher_task_status, pdFALSE, eInvalid);
+    printf("Lower Task Status:\n\tState: %d\n\tName: %s\n\tExecution Time: %llu\n", lower_task_status.eCurrentState, lower_task_status.pcTaskName, lower_task_status.ulRunTimeCounter);
+    printf("Medium Task Status:\n\tState: %d\n\tName: %s\n\tExecution Time: %llu\n", medium_task_status.eCurrentState, medium_task_status.pcTaskName, medium_task_status.ulRunTimeCounter);
+    printf("Higher Task Status:\n\tState: %d\n\tName: %s\n\tExecution Time: %llu\n", higher_task_status.eCurrentState, higher_task_status.pcTaskName, higher_task_status.ulRunTimeCounter);
     }
+
+    vTaskDelete(lower_task);
+    vTaskDelete(medium_task);
+    vTaskDelete(higher_task);
 }
 
 void runner_thread (__unused void *args)
